67deba06a6b334907095bf989036743a03f6a4ee
triduc11 <143623017+triduc11@users.noreply.github.com>
2023-12-13 17:45:41 +0700

    update ss8

---
+++ Module_2/bai_tap/ss8/src/bai_1_Refactoring/TennisGame.java
@@ -0,0 +1,64 @@
+package bai_1_Refactoring;
+
+public class TennisGame {
+    public static String getScore(String player1Name, String player2Name, int mScore1, int mScore2) {
+        String score = "";
+        int tempScore = 0;
+        if (mScore1 == mScore2) {
+            switch (mScore1) {
+                case 0:
+                    score = "Love-All";
+                    break;
+                case 1:
+                    score = "Fifteen-All";
+                    break;
+                case 2:
+                    score = "Thirty-All";
+                    break;
+                case 3:
+                    score = "Forty-All";
+                    break;
+                default:
+                    score = "Deuce";
+                    break;
+
+            }
+        } else if (mScore1 >= 4 || mScore2 >= 4) {
+            int minusResult = mScore1 - mScore2;
+            if (minusResult == 1) {
+                score = "Advantage player1";
+            } else if (minusResult == -1) {
+                score = "Advantage player2";
+            } else if (minusResult >= 2) {
+                score = "Win for player1";
+            } else {
+                score = "Win for player2";
+
+            }
+        } else {
+            for (int i = 1; i < 3; i++) {
+                if (i == 1) tempScore = mScore1;
+                else {
+                    score += "-";
+                    tempScore = mScore2;
+                }
+                switch (tempScore) {
+                    case 0:
+                        score += "Love";
+                        break;
+                    case 1:
+                        score += "Fifteen";
+                        break;
+                    case 2:
+                        score += "Thirty";
+                        break;
+                    case 3:
+                        score += "Forty";
+                        break;
+                }
+            }
+        }
+        return score;
+    }
+}
+
